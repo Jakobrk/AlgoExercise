f34abe166a83fa87b3d1c6e1d5061fc211739026
jakobrk <jakobrk@gmail.com>
2025-12-13 00:26:00 +0100

    week 2 af code forces solved

--- CodeForces/2169B.py
+++ CodeForces/2169B.py
@@ -1,50 +1,83 @@
-#https://codeforces.com/problemset/problem/2169/B?fbclid=IwZXh0bgNhZW0CMTAAYnJpZBEwOTRRYmd3NXhTa2FHRTQwcXNydGMGYXBwX2lkEDIyMjAzOTE3ODgyMDA4OTIAAR4pVG6EAzFJa0__xm1-fUi94Fyneam-LDXEZR-RqfymtcGzHYXm25MH1LtCJQ_aem_8bnXlZLNqPNpSX-2FWVx4A
-
-
-
 test_cases = int(input())
 
-
-for ppp in range(test_cases):
-
-    river = list(input())
-    n = len(river)
+for _ in range(test_cases):
+    s = input().strip()
+    n = len(s)
     
-    river = ["L"] + river + ["R"]
-
-    print(river)
-    #print(n)
-
-    max_strokes = 0
-
-    for start_pos in range(n+2):
-        if river[start_pos] == "L" or river[start_pos] == "R":
-            continue
-        
-        #altid uendeligt
-        if river[start_pos] == "*":
-            if river[start_pos-1] == ">" or river[start_pos+1] == "<" or river[start_pos-1] == "*" or river[start_pos+1] == "*":
-                max_strokes = -1
+    # Check for om der findes uendelig-svømmetid
+    infinite = False
+    
+    for i in range(n):
+        if s[i] == '*':
+            # * ved siden af *
+            if i > 0 and s[i-1] == '*':
+                infinite = True
                 break
-
-        #string er 1 lang
-        if river[start_pos-1] == "L" and river[start_pos+1] == "R":
-            max_strokes == 1
-            break
-
+            if i < n-1 and s[i+1] == '*':
+                infinite = True
+                break
+            # >*,  strømmen leder til en stjerne vi kan svømme tilbage fra
+            if i > 0 and s[i-1] == '>':
+                infinite = True
+                break
+            # *< modsat ovenfor
+            if i < n-1 and s[i+1] == '<':
+                infinite = True
+                break
         
+        # ><, vi can bounce uendeligt
+        if s[i] == '>' and i < n-1 and s[i+1] == '<':
+            infinite = True
+            break
+        
+        # ><, fra den anden side  
+        if s[i] == '<' and i > 0 and s[i-1] == '>':
+            infinite = True
+            break
     
-
-
-
-
-
-
-
-
-
-
-    print(max_strokes)
+    if infinite:
+        print(-1)
+        continue
+    
+    # Find længste sequence en vej
+    # Stjener kan være wildcards
+    max_length = 0
+    
+    # Find længste mod right 
+    i = 0
+    while i < n:
+        if s[i] in '>*':
+            count = 0
+            # Tæl så længe vi ser > eller *
+            while i < n and s[i] in '>*':
+                count += 1
+                i += 1
+            # Kun tæl sekvensen hvis den indeholder mindst én >
+            if '>' in s[i-count:i]:
+                max_length = max(max_length, count)
+        else:
+            i += 1
+    
+    # Find længste mod venstre 
+    i = 0
+    while i < n:
+        if s[i] in '<*':
+            count = 0
+            # Tæl så længe vi ser < eller *
+            while i < n and s[i] in '<*':
+                count += 1
+                i += 1
+            # Kun tæl sekvensen hvis den indeholder mindst én <
+            if '<' in s[i-count:i]:
+                max_length = max(max_length, count)
+        else:
+            i += 1
+    
+    # Edge case: string er kun én stjerne
+    if n == 1 and s[0] == '*':
+        max_length = 1
+    
+    print(max_length)
 
 
 
